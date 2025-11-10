67cc9a3233d1d5f82f0fa54141d0b0745fa000cf
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-11-10 13:17:12 +0530

    Add initial project structure with configuration, main application, and command package
    - Created .air.toml for build configuration.
    - Added go.mod for module definition.
    - Implemented main application in tutorial.go.
    - Introduced cmd package with Connect and GetUserData functions.
    - Included binary files for the main executable in build and tmp directories.

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package cmd
+
+import(
+	"fmt"
+)
+
+func Connect() {
+	fmt.Println("Read from cmd")
+}
+
+func GetUserData(){
+	fmt.Println("User data loading...")
+}
---
+++ tutorial.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/hello/cmd"
+)
+
+// build command - go build -o ./build/main.exe
+func main() {
+	fmt.Println("Hello From Go...!")
+	var name string = "Sachin"
+	fmt.Println(name)
+
+	pageCount := 499
+	fmt.Println(pageCount)
+
+	pageCount = 500
+	fmt.Println(pageCount)
+
+	age := 18
+
+	if age >= 18 {
+		fmt.Println("You are old enough to vote", age)
+	} else {
+		fmt.Println("You need to be at least 18 or above to vote.")
+	}
+
+	// use other go files
+	cmd.Connect()
+	cmd.GetUserData()
+}
+
+// Double shift to switch between multiple files
